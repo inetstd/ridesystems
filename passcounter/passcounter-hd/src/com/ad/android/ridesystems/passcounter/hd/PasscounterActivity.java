f5d0b396c06b2d604f8d3213607e03b646df3cf3
Victor Melnik <victor@inetstd.com>
2012-06-15 09:58:10 +0300

    hd version

---
+++ passcounter/passcounter-hd/src/com/ad/android/ridesystems/passcounter/hd/PasscounterActivity.java
@@ -0,0 +1,13 @@
+package com.ad.android.ridesystems.passcounter.hd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PasscounterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ passcounter/passcounter-mob/src/com/ad/android/ridesystems/passengercounter/mob/PasscounterActivity.java
@@ -0,0 +1,13 @@
+package com.ad.android.ridesystems.passengercounter.mob;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PasscounterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
